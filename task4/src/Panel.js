92c93b7851e9326e590ce942cbcc55df617155f9
Maira Bello <maira.tma@gmail.com>
2015-06-08 21:47:03 -0300

    Adds task 4 contents

---
+++ task4/src/Panel.js
@@ -0,0 +1,28 @@
+import ComponentRegistry from 'bower:metal/src/component/ComponentRegistry';
+import SoyComponent from 'bower:metal/src/soy/SoyComponent';
+import './Panel.soy';
+
+class Panel extends SoyComponent {
+	constructor(opt_config) {
+		super(opt_config);
+	}
+
+	hide() {
+		this.visible = false;
+	}
+}
+
+Panel.ATTRS = {
+	title: {
+		value: 'Title'
+	},
+	body: {
+		value: 'Body'
+	}
+};
+
+Panel.ELEMENT_CLASSES = 'panel panel-default';
+
+ComponentRegistry.register('Panel', Panel);
+
+export default Panel;
